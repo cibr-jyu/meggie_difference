1dfdf9b6973529b470e93be1c936a738ccecd192
teekuningas <erkkahe@gmail.com>
2025-03-08 18:39:58 +0200

    Add spectrum difference objects

--- meggie_difference/actions/evoked_difference/tests/test_evoked_difference.py
+++ meggie_difference/actions/evoked_difference/tests/test_evoked_difference.py
@@ -1,3 +1,11 @@
+import mne
+
+from meggie.utilities.testing import create_evoked_conditions_experiment
+from meggie.utilities.events import find_events
+from meggie.utilities.events import find_stim_channel
+from meggie.datatypes.epochs.epochs import Epochs
+from meggie.datatypes.evoked.evoked import Evoked
+
 from meggie_difference.utilities.testing import BaseDifferenceTestAction
 from meggie_difference.actions.evoked_difference import EvokedDifference
 from meggie_difference.utilities.dialogs.differenceDialogMain import (
@@ -5,7 +13,82 @@ from meggie_difference.utilities.dialogs.differenceDialogMain import (
 )
 
 
+def create_difference_experiment(experiment_folder, experiment_name, n_subjects=2):
+    """Generate experiment with data for testing."""
+
+    experiment = create_evoked_conditions_experiment(
+        experiment_folder, experiment_name, n_subjects=n_subjects
+    )
+
+    # create trivial content
+    for subject in experiment.subjects.values():
+        raw = subject.get_raw()
+
+        # create epochs
+        stim_channel = find_stim_channel(raw)
+        events = find_events(raw, stim_channel)
+
+        epochs_directory = subject.epochs_directory
+
+        params = {
+            "tmin": -0.1,
+            "tmax": 0.2,
+            "bstart": -0.1,
+            "bend": 0.2,
+        }
+        category = {"1": 1}
+        mne_epochs_1 = mne.Epochs(
+            raw,
+            events,
+            category,
+            tmin=params["tmin"],
+            tmax=params["tmax"],
+            baseline=(params["bstart"], params["bend"]),
+        )
+        epochs_1 = Epochs("Epochs", epochs_directory, params, content=mne_epochs_1)
+        epochs_1.save_content()
+        subject.add(epochs_1, "epochs")
+
+        params = {
+            "tmin": -0.1,
+            "tmax": 0.2,
+            "bstart": -0.1,
+            "bend": 0.2,
+        }
+        category = {"2": 2}
+
+        mne_epochs_2 = mne.Epochs(
+            raw,
+            events,
+            category,
+            tmin=params["tmin"],
+            tmax=params["tmax"],
+            baseline=(params["bstart"], params["bend"]),
+        )
+        epochs_directory = subject.epochs_directory
+        epochs_2 = Epochs("Epochs", epochs_directory, params, content=mne_epochs_2)
+        epochs_2.save_content()
+        subject.add(epochs_2, "epochs")
+
+        # create evoked with two conditions
+        params = {"conditions": ["epochs_1", "epochs_2"]}
+        content = {
+            "epochs_1": mne_epochs_1.average(),
+            "epochs_2": mne_epochs_2.average(),
+        }
+        evoked_directory = subject.evoked_directory
+        evoked = Evoked("Evoked", evoked_directory, params, content=content)
+        evoked.save_content()
+        subject.add(evoked, "evoked")
+
+    return experiment
+
+
 class TestEvokedDifference(BaseDifferenceTestAction):
+
+    def setup_experiment(self):
+        self.experiment = create_difference_experiment(self.dirpath, "test_experiment")
+        self.experiment.activate_subject("sample_01-raw")
 
     def test_evoked_difference(self):
 
